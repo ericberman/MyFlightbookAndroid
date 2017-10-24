8a933339eca2e7b320106e03b3686035f135db10
Eric Berman <ericberman@users.noreply.github.com>
2017-10-24 08:35:03 -0700

    update airport database

--- app/src/main/java/Model/MFBConstants.java
+++ app/src/main/java/Model/MFBConstants.java
@@ -35,7 +35,7 @@ public class MFBConstants {
 
     // DB Versioning
     public static final int DBVersionMain = 22;
-    public static final int DBVersionAirports = 21;
+    public static final int DBVersionAirports = 22;
 
     // To set the DB version in Sqlite: PRAGMA user_version = x.  BE SURE TO DO THIS OR ELSE COPY DATABASE WILL NOT WORK
     // To read it: PRAGMA user_version
