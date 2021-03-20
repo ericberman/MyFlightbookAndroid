7cced89d169783e2721765ad15b503283fcd7629
Eric Berman <ericberman@users.noreply.github.com>
2021-03-20 08:34:03 -0700

    Always refresh the aircraft list.

--- app/src/main/java/com/myflightbook/android/ActNewFlight.java
+++ app/src/main/java/com/myflightbook/android/ActNewFlight.java
@@ -738,13 +738,7 @@ public class ActNewFlight extends ActMFBForm implements android.view.View.OnClic
             d.show();
         }
 
-        if (m_rgac == null) {
-            Aircraft[] rgac = (new AircraftSvc()).getCachedAircraft();
-            if (rgac != null) {
-                m_rgac = rgac;
-                refreshAircraft(m_rgac, false);
-            }
-        }
+        refreshAircraft(m_rgac == null ? (new AircraftSvc()).getCachedAircraft() : m_rgac, false);
 
         // fix up the link to the user's profile.
         AddListener(R.id.txtSocialNetworkHint);
