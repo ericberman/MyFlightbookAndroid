55945a3b8f0f84a268d30e0986bcc30a298cf604
Eric Berman <ericberman@users.noreply.github.com>
2023-02-13 09:25:13 -0800

    Fix permissioning issue for training tab.

--- app/src/main/java/com/myflightbook/android/ActTraining.kt
+++ app/src/main/java/com/myflightbook/android/ActTraining.kt
@@ -1,7 +1,7 @@
 /*
 	MyFlightbook for Android - provides native access to MyFlightbook
 	pilot's logbook
-    Copyright (C) 2017-2022 MyFlightbook, LLC
+    Copyright (C) 2017-2023 MyFlightbook, LLC
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -130,9 +130,13 @@ class ActTraining : ListFragment(), OnItemClickListener {
             if (fNeedsWritePerm) arrayOf(
                 Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
-            ) else arrayOf(
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            )
+            ) else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                arrayOf(
+                    Manifest.permission.READ_MEDIA_IMAGES
+                )
+            } else {
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
         )
     }
 
