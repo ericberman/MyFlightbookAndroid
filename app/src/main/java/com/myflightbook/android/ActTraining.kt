4d7f9fbb102674ed482eb9ee209e3d95b9f7f215
Eric Berman <ericberman@users.noreply.github.com>
2026-08-05 18:36:31 -0700

    Fix permissioning that broke training on Tiramisu

--- app/src/main/java/com/myflightbook/android/ActTraining.kt
+++ app/src/main/java/com/myflightbook/android/ActTraining.kt
@@ -1,7 +1,7 @@
 /*
 	MyFlightbook for Android - provides native access to MyFlightbook
 	pilot's logbook
-    Copyright (C) 2017-2025 MyFlightbook, LLC
+    Copyright (C) 2017-2026 MyFlightbook, LLC
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,9 +18,7 @@
  */
 package com.myflightbook.android
 
-import android.Manifest
 import android.content.Context
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -29,8 +27,6 @@ import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
 import android.widget.ArrayAdapter
 import android.widget.TextView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions
 import androidx.fragment.app.ListFragment
 import com.myflightbook.android.webservices.AuthToken.Companion.isValid
 import com.myflightbook.android.webservices.MFBSoap.Companion.isOnline
@@ -38,8 +34,6 @@ import model.MFBConstants.authRedirWithParams
 import model.MFBUtil.alert
 
 class ActTraining : ListFragment(), OnItemClickListener {
-    private var mPermissionLauncher: ActivityResultLauncher<Array<String>>? = null
-
     internal class TrainingItem(val idTitle: Int, val szURLDest: String)
 
     private val mRgtrainingitems = arrayOf(
@@ -80,29 +74,6 @@ class ActTraining : ListFragment(), OnItemClickListener {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mPermissionLauncher = registerForActivityResult(
-            RequestMultiplePermissions()
-        ) { result: Map<String, Boolean>? ->
-            if (result != null) {
-                var fAllGranted = true
-                for (sz in result.keys) {
-                    val b = result[sz] ?: false
-                    if (sz.compareTo(Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED) == 0 && b) {
-                        fAllGranted = true
-                        break
-                    }
-                    fAllGranted = fAllGranted && b
-                }
-
-                // perform the actual click.
-                val position = lastPositionClicked
-                lastPositionClicked = -1 // clear this out now.
-                if (position >= 0 && position < mRgtrainingitems.size && fAllGranted) {
-                    val szDest = mRgtrainingitems[position].szURLDest
-                    ActWebView.viewURL(requireActivity(), authRedirWithParams("d=$szDest", context))
-                }
-            }
-        }
         return inflater.inflate(R.layout.training, container, false)
     }
 
@@ -118,37 +89,16 @@ class ActTraining : ListFragment(), OnItemClickListener {
         listView.onItemClickListener = this
     }
 
-    private var lastPositionClicked = -1
     override fun onItemClick(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
         // TODO: IsOnline doesn't work from main thread.
         if (!isValid() || !isOnline(context)) {
             alert(this, getString(R.string.txtError), getString(R.string.errTrainingNotAvailable))
             return
         }
-        lastPositionClicked = position
-
-        // check for permissions and handle click there.
-        // See https://developer.android.com/reference/android/Manifest.permission#READ_EXTERNAL_STORAGE
-        // If I understand the ever-changing Android requirements:
-        // - Until Q (29), you need to request READ_EXTERNAL_STORAGE
-        // - From Q (29) until Tiramasu (33), you don't need any permissions to read the user's media
-        // From Tiramasu (33) onward, use READ_MEDIA_IMAGES/READ_MEDIA_VIDEO, not READ_EXTERNAL_STORAGE
-        mPermissionLauncher!!.launch(
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-                arrayOf(
-                    Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED,
-                    Manifest.permission.READ_MEDIA_IMAGES,
-                    Manifest.permission.READ_MEDIA_VIDEO
-                )
-            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                arrayOf(
-                    Manifest.permission.READ_MEDIA_IMAGES,
-                    Manifest.permission.READ_MEDIA_VIDEO
-                )
-            } else {
-                arrayOf()
-            }
-        )
+        if (position >= 0 && position < mRgtrainingitems.size) {
+            val szDest = mRgtrainingitems[position].szURLDest
+            ActWebView.viewURL(requireActivity(), authRedirWithParams("d=$szDest", context))
+        }
     }
 
     companion object {
